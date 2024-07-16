bf1f1d6ce7f19cde034c013fbee935aa7e74f562
unknown <kartik.agg2407@gmail.com>
2024-07-16 17:16:08 +0530

    Fourth commit

--- assignment1/q20.java
+++ assignment1/q20.java
@@ -1,12 +1,17 @@
 package assignment1;
 
-public class q19 {
+public class q20 {
     public static void main(String[] args) {
         int n = 7 ;
         int a = (n+1)/2 ; 
         for (int i=a ; i>=1 ; i--){
             for (int j=1 ; j<=i ; j++){
-                System.out.print("* ");
+                if (j==i){
+                    System.out.print("* ");
+                }
+                else{
+                    System.out.print("  ");
+                }
             }
             for (int k=1 ; k<=a-i ; k++){
                 System.out.print("  ");
@@ -14,14 +19,19 @@ public class q19 {
             for (int k=2 ; k<=a-i  ; k++){
                 System.out.print("  ");
             }
-            for (int j=1 ; j<=i && j<a; j++){
+            for (int j=1 ; j<=a-i && j==1; j++){
                 System.out.print("* ");
             }
             System.out.println();
         }   
         for (int i=2 ; i<=a ; i++){
             for (int j=1 ; j<=i ; j++){
-                System.out.print("* ");
+                if (j==i){
+                    System.out.print("* ");
+                }
+                else{
+                    System.out.print("  ");
+                }
             }
             for (int k=a ; k>=i+1 ; k--){
                 System.out.print("  ");
@@ -29,10 +39,10 @@ public class q19 {
             for (int k=a-1 ; k>=i+1 ; k--){
                 System.out.print("  ");
             }
-            for (int j=1 ; j<=i && j<=a-1; j++){
+            for (int j=1 ; j<=i && i<a && j==1 ; j++){
                 System.out.print("* ");
             }
             System.out.println();
         }
-    }   
+    }
 }
