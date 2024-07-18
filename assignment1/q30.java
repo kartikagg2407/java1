72990f96acc97dd305989a3eaa2d5a01154c3194
unknown <kartik.agg2407@gmail.com>
2024-07-18 16:41:27 +0530

    seventh commit

---
+++ assignment1/q30.java
@@ -0,0 +1,13 @@
+package assignment1;
+
+public class q30 {
+    public static void main(String[] args) {
+        int n = 5 ;
+        for (int i=1 ; i<=n ; i++){
+            for (int j=1 ; j<=n ; j++){
+                System.out.print(n-j+1 + "  ");
+            }
+            System.out.println();
+        }
+    }
+}
