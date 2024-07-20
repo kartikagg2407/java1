3598c4a6b4632283344fae6fbf0d353378eac06a
unknown <kartik.agg2407@gmail.com>
2024-07-20 12:26:19 +0530

    array

---
+++ array.java
@@ -0,0 +1,6 @@
+public class array {
+    public static void main(String[] args) {
+        char [] arr= new char[10];
+        System.out.println(arr);
+    }
+}
---
+++ function5.java
@@ -0,0 +1,14 @@
+public class function5 {
+    static int digits(int n){
+        int s=0;
+        for (int i=1 ; i<=n*10 ; i++){
+            n/=10;
+            s++;
+        }
+        return s;
+    }
+    public static void main(String[] args) {
+        int n =12830;
+        System.out.println(digits(n));
+    }
+}
