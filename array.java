36b7848cb2836108dadc9cc41adca988a1bf9e1f
unknown <kartik.agg2407@gmail.com>
2024-07-21 14:11:59 +0530

    array

--- array.java
+++ array.java
@@ -1,24 +1,21 @@
-import java.util.*;
+
 public class array {
-    static int sum(int[] arr){
+    static int sum(int[] arr , int a , int b){
         int s=0 ; 
-        for (int i:arr){
-            s+=i;
+        if (a>b || b>arr.length || a<0 || b<0){
+            return s;
+        }
+        for (int i=a ; i<=b ; i++){
+            s+=arr[i];
         }
         return s;
     } 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the size of the array : ");
-        int a=sc.nextInt();
-        int [] num= new int [a];
+        int [] num= new int [5];
         int b = num.length;
         for (int i=0 ; i<b ; i++){
-            System.out.print("Enter the value of "+ (i+1) + " th index : ");
-            int c = sc.nextInt();
-            num[i] = c;
+            num[i] = i+1;
         }
-        System.out.println("The sum of number in array is : " + sum(num));
-        sc.close();
+        System.out.println("The sum of number in array is : " + sum(num , 1 , 3));
     }
 }
--- array2.java
+++ array2.java
@@ -1,4 +1,3 @@
-import java.util.*;
 public class array2 {
     static void reverse_array(int[] arr){
         int a=arr.length;
@@ -7,17 +6,10 @@ public class array2 {
         }
     }
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the size of the array : ");
-        int a=sc.nextInt();
-        int [] num= new int [a];
-        int b = num.length;
-        for (int i=0 ; i<b ; i++){
-            System.out.print("Enter the value of "+ (i+1) + " th index : ");
-            int c = sc.nextInt();
-            num[i] = c;
+        int [] num= new int [5];
+        for (int i=0 ; i<5 ; i++){
+            num[i] = i+1;
         }
         reverse_array(num);
-        sc.close();
     }
 }
